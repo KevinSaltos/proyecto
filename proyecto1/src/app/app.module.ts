d596e8994a96d5731f112013179f286812ba53d9
KEVIN SANI <kevinsani12@outlook.com>
2022-11-17 17:12:25 -0500

    avance

---
+++ proyecto1/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { UserComponent } from './user/user/user.component';
+import { RegisterComponent } from './user/register/register.component';
+import { HeaderComponent } from './static/header/header.component';
+import { NavComponent } from './static/nav/nav.component';
+import { FooterComponent } from './static/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    UserComponent,
+    RegisterComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
